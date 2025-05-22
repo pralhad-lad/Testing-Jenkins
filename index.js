58bcf187b364cc6005a9057cb0c88de7ede128f1
Pralhad Lad <168797310+pralhad-lad@users.noreply.github.com>
2025-05-22 06:57:48 +0530

    added another function index.js file

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function greet(name) {
   console.log("Hello, " + name + "!");
 }
 greet("Jenkins");
+
+function greetTo(lname){
+  console.log("Hello, " + lname + "What's App man/women");
+}
+greetTo("Pralhad");
